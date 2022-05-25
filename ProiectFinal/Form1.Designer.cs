3b23d9c7443e35cfc267f71778cb125bccfe290a
Munteanu Teofil <andreiteofil01@gmail.com>
2022-05-25 23:22:22 +0300

    Anchor Top

--- ProiectFinal/Form1.Designer.cs
+++ ProiectFinal/Form1.Designer.cs
@@ -192,9 +192,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.Location = new System.Drawing.Point(29, 98);
             this.pictureBox1.Name = "pictureBox1";
