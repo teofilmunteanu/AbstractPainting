e43733b5d2082b349099d3d881e09345e9ef70be
Munteanu Teofil <andreiteofil01@gmail.com>
2022-06-05 21:56:56 +0300

    Diagram

--- ProiectFinal/Info.Designer.cs
+++ ProiectFinal/Info.Designer.cs
@@ -29,15 +29,19 @@ namespace ProiectFinal
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Info));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBoxInfo = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(103, 70);
+            this.label1.Location = new System.Drawing.Point(12, 72);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(197, 23);
             this.label1.TabIndex = 0;
@@ -47,23 +51,45 @@ namespace ProiectFinal
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(103, 120);
+            this.label2.Location = new System.Drawing.Point(12, 122);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(115, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "Grupa: M524";
+            // 
+            // pictureBoxInfo
+            // 
+            this.pictureBoxInfo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxInfo.Image")));
+            this.pictureBoxInfo.Location = new System.Drawing.Point(229, 72);
+            this.pictureBoxInfo.Name = "pictureBoxInfo";
+            this.pictureBoxInfo.Size = new System.Drawing.Size(604, 372);
+            this.pictureBoxInfo.TabIndex = 2;
+            this.pictureBoxInfo.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(497, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ierarhie";
             // 
             // Info
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(395, 235);
+            this.ClientSize = new System.Drawing.Size(845, 468);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBoxInfo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Info";
             this.Text = "Info";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Info_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInfo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -73,5 +99,7 @@ namespace ProiectFinal
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBoxInfo;
+        private System.Windows.Forms.Label label3;
     }
 }
--- ProiectFinal/Info.cs
+++ ProiectFinal/Info.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,8 @@ namespace ProiectFinal
         private Info()
         {
             InitializeComponent();
+
+            pictureBoxInfo.Image = resizeImage(pictureBoxInfo.Image, pictureBoxInfo.Width, pictureBoxInfo.Height);
         }
 
         public static Info getInfo()
@@ -31,6 +35,32 @@ namespace ProiectFinal
         private void Info_FormClosed(object sender, FormClosedEventArgs e)
         {
             instance = null;
+        }
+
+        static Bitmap resizeImage(Image img, int width, int height)
+        {
+            Bitmap newImage = new Bitmap(width, height);
+            Rectangle r = new Rectangle(0, 0, width, height);
+
+            newImage.SetResolution(img.HorizontalResolution, img.VerticalResolution);
+            Graphics graphics = Graphics.FromImage(newImage);
+            
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            var wrap = new ImageAttributes();
+            wrap.SetWrapMode(WrapMode.TileFlipXY);
+            graphics.DrawImage(img, r, 0, 0, img.Width, img.Height, GraphicsUnit.Pixel, wrap);
+
+            return newImage;
+        }
+
+        public void Method()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
